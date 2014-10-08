8d13ca447b63cdc1da242a3317acba678a259100
Michael Hulet <bestraysfan@me.com>
2014-10-07 21:13:03 -0400

    Refactored timeColor.js and the site's Sass/CSS to support a background rotating around the HSL Color Wheel, instead of simply a random color

--- Placeholder/js/timeColor.js
+++ Placeholder/js/timeColor.js
@@ -8,75 +8,128 @@ function RGBToHex(r,g,b){
 function getRandomColorValue(){
 	return Math.round(Math.random()*255);
 }
+function hour(input, date){
+	if(input==="number"){
+		var hours;
+		if(date.getHours()>12){
+			return date.getHours()-12;
+		}
+		else if(date.getHours()===0){
+			return 12;
+		}
+		else{
+			return date.getHours();
+		}
+	}
+	else if(input==="ap"){
+		if(date.getHours()>=12){
+			return 1;
+		}
+		else{
+			return 0;
+		}
+	}
+}
+function minutes(date){
+	if(date.getMinutes()<10){
+		return "0"+date.getMinutes();
+	}
+	else{
+		return date.getMinutes();
+	}
+}
+function seconds(date){
+	if(date.getSeconds()<10){
+		return "0"+date.getSeconds();
+	}
+	else{
+		return date.getSeconds();
+	}
+}
+function getAndAssignTime(){
+	var date = new Date();
+}
+function RGBToHSL(r, g, b){
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+    if(max == min){
+        h = s = 0; // achromatic
+    }else{
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+            case r:
+							h = (g - b) / d + (g < b ? 6 : 0);
+							break;
+            case g:
+							h = (b - r) / d + 2;
+							break;
+            case b:
+							h = (r - g) / d + 4;
+							break;
+        }
+        h /= 6;
+    }
+    return [Math.round(h * 360), Math.round(s * 100), Math.round(l * 100)];
+}
+function HueToRGB(m1, m2, hue){
+	if(hue < 0){
+		hue += 1;
+	}
+	if(hue > 1){
+		hue -=1;
+	}
+	if(hue * 6 < 1){
+		return m1 + (m2 - m1) * hue * 6;
+	}
+	if(hue * 2 < 1){
+		return m2;
+	}
+	if(hue * 3 < 2){
+		return m1 + (m2 - m1) * (2/3 - hue) * 6;
+	}
+	return m1;
+}
+function HSLToRGB(h, s, l){
+	h /= 360;
+	s /= 100;
+	l /= 100;
+	var m1, m2, r, g, b;
+  if(l <= 0.5){
+		m2 = l * (s + 1);
+	}
+	else{
+		m2 = l + s - l * s;
+	}
+	m1 = l * 2 - m2;
+	r = HueToRGB(m1, m2, h + 1/3);
+	g = HueToRGB(m1, m2, h);
+	b = HueToRGB(m1, m2, h - 1/3);
+	return [r, g, b];
+}
 var days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 var months=["January","February","March","April","May","June","July","August","September","October","November","December"];
 var gmt=["AM","PM"];
-window.onload = function(){changeColorAndTime();}
+var prevColor = RGBToHSL(34, 177, 76);
+window.onload = function(){changeColorAndTime();};
 setInterval(function(){
 	changeColorAndTime();
-}, 1000);
+}, 100);
 function changeColorAndTime(){
 	var date = new Date();
-	function hour(input){
-		if(input==="number"){
-			var hours;
-			if(date.getHours()>12){
-				return date.getHours()-12;
-			}
-			else if(date.getHours()===0){
-				return 12;
-			}
-			else{
-				return date.getHours();
-			}
-		}
-		else if(input==="ap"){
-			if(date.getHours()>=12){
-				return 1;
-			}
-			else{
-				return 0;
-			}
-		}
+	document.getElementsByTagName('html')[0].style.background = "hsl("+prevColor[0]+", "+prevColor[1]+"%, "+prevColor[2]+"%)";
+	document.getElementById('date').innerHTML=days[date.getDay()]+", "+months[date.getMonth()]+" "+date.getDate()+", "+date.getFullYear()+" "+hour("number", date)+":"+minutes(date)+":"+seconds(date)+" "+gmt[hour("ap", date)];
+	var color = HSLToRGB(prevColor[0], prevColor[1], prevColor[2]);
+	for(var i = 0; i< 3; i++){
+		color[i] = Math.round(color[i]*255);
 	}
-	function minutes(){
-		if(date.getMinutes()<10){
-			return "0"+date.getMinutes();
-		}
-		else{
-			return date.getMinutes();
-		}
-	}
-	function seconds(){
-		if(date.getSeconds()<10){
-			return "0"+date.getSeconds();
-		}
-		else{
-			return date.getSeconds();
-		}
-	}
-	var r = getRandomColorValue();
-	var g = getRandomColorValue();
-	var b = getRandomColorValue();
-	if(r+g+b<383 && g<175){
-		for(var i = 0; i < document.getElementsByTagName('p').length; i++){
-			document.getElementsByTagName('p')[i].style.color = "#ccc";
-		}
-		document.getElementsByTagName('header')[0].style.boxShadow = "0 0 10px 1px #ccc";
-		document.getElementsByTagName('h2')[0].style.color = "#ccc";
+	document.getElementById('color').innerHTML = RGBToHex(color[0], color[1], color[2]);
+	document.getElementById('rgb').innerHTML = "(rgb: "+color[0]+", "+color[1]+", "+color[2]+")";
+	if(prevColor[0]===360){
+		prevColor[0] = 0;
 	}
 	else{
-		for(var x = 0; x < document.getElementsByTagName('p').length; x++){
-			document.getElementsByTagName('p')[x].style.color = "#000";
-		}
-		document.getElementsByTagName('header')[0].style.boxShadow = "0 0 10px 1px #000";
-		document.getElementsByTagName('h2')[0].style.color = "#000";
+		prevColor[0]++;
 	}
-	var color = RGBToHex(r,g,b);
-	document.getElementById('color').innerHTML=color;
-	document.getElementById('date').innerHTML=days[date.getDay()]+", "+months[date.getMonth()]+" "+date.getDate()+", "+date.getFullYear()+" "+hour("number")+":"+minutes()+":"+seconds()+" "+gmt[hour("ap")];
-	var rgb = "(rgb: "+r+", "+g+", "+b+")";
-	document.getElementById('rgb').innerHTML=rgb;
-	document.getElementsByTagName("html")[0].style.background=color;
-	console.log(color+", "+ rgb);
 }
